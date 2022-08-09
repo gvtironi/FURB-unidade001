602eba8e641281cb4407897bf55cbd7412b40258
Gustavo Vinícius Tironi <110572435+gvtironi@users.noreply.github.com>
2022-08-09 19:59:49 -0300

    Add files via upload

---
+++ furb004.java
@@ -0,0 +1,9 @@
+public class furb004 {
+    public static void main(String[] args) {
+        //entradas
+        int numero1 = 10;
+        int numero2 = 2;
+        //processo
+        int soma = numero1 + numero2;
+        System.out.println(soma);
+} }
